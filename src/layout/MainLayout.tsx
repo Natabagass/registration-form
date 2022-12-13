076ecd4d7e3d4812d043bbe8d107c45666705cd1
Natabagas <meganatabagas@gmail.com>
2022-12-13 09:29:57 +0700

    first commit

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+import { ReactNode } from "react";
+
+export default function MainLayout({ children }: { children: ReactNode }) {
+	return (
+		<>
+			<Header />
+			{children}
+		</>
+	);
+}
