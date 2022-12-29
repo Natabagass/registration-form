df5da54d42b951c47b1cc95693d5f1a3690e3a1d
Natabagas <meganatabagas@gmail.com>
2022-12-29 14:20:46 +0700

    styling and fixing price

--- src/components/FourthStep.tsx
+++ src/components/FourthStep.tsx
@@ -4,10 +4,12 @@ import {BsFillPersonFill, BsCalendarRangeFill} from "react-icons/bs"
 import {FaCoins, FaPen} from "react-icons/fa"
 import { jam } from "../dummy-data";
 import {IoTicket} from 'react-icons/io5'
+import CurrencyFormat from 'react-currency-format';
 // import Datepicker from 'flowbite-datepicker/Datepicker';
 
 export default function FourthStep() {
     const {setStep, userData, setUserData, submitData} = useContext(multiStepContext)
+    const [price, setPrice] = useState(0);
     const count = 6;
     const arrayCounter = Array.from(Array(count).keys());
     const chooseCounter = userData.count;
@@ -25,7 +27,7 @@ export default function FourthStep() {
                         <div className='flex justify-start'>
                             <div className='border flex flex-col w-[358px] border-[#B1C4F0] mt-[10px] rounded-xl'>
                                 <div className='p-5'>
-                                    <p className='text-[#5885E9] font-bold'>Rp. 260.000 <span className='font-normal text-[#A7A7A7]'>/sesi</span></p>
+                                    <div className='text-[#5885E9] font-bold'>Rp. {price} <span className='font-normal text-[#A7A7A7]'>/sesi</span></div>
                                     <div className='flex flex-row'>
                                         <div className='flex flex-row mt-[5px] text-[16px] text-[#FFAE5F]'><BsFillPersonFill/><span className='-mt-[4px] text-[#727272] ml-[5px] text-[13px]'>1-5 orang</span></div>
                                         <div className='mx-auto flex justify-end w-[180px] -mt-[20px] text-[#B1C4F0] text-[31px]'><FaCoins/></div>
@@ -46,6 +48,8 @@ export default function FourthStep() {
                                             e.preventDefault()
                                             setUserData({...userData, "count" : item === 0 ? item + 1 : item + item});
                                             setCounterArrays(Array.from(Array(item === 0 ? item + 1 : item + item).keys()));
+                                            setPrice(item === 0 ? (item + 1) * 260000 :
+                                                item < 3  ?  (item + item) * 260000 : (item + item) * 300000)
                                         }} 
                                         className='border px-5 py-3 rounded-xl hover:bg-[#F5F7FF] mt-[10px] active:bg-[#5885E9] active:text-white mr-3'>
                                             {item === 0 ? item + 1 : item + item}
@@ -130,12 +134,13 @@ export default function FourthStep() {
 
                         <div className='mt-[50px] '> 
                             <label className='font-bold text-[16px]'>Upload File Tugas</label>
-                            <div className="flex items-center mt-[30px] justify-center w-full">
-                                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2  border-dashed rounded-lg cursor-pointer bg-gray-50  hover:bg-gray-100">
+                            <div className="flex items-center mt-[30px] justify-start w-full">
+                                <label htmlFor="dropzone-file" className="flex flex-col border-[#B1C4F0] items-center justify-center w-[300px] h-[400px] border-[8px] border-dashed rounded-lg cursor-pointer ">
                                     <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                        <svg aria-hidden="true" className="w-10 h-10 mb-3 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                                        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-[70px] h-[84px] text-[#5885E9]"><path fill-rule="evenodd" d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5H5.625zM7.5 15a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 017.5 15zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H8.25z" clip-rule="evenodd" /><path d="M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z" /></svg>
+                                        <p className="mb-2 text-[23px] font-Cairo text-[#A7A7A7] mt-[20px]">Drop file tugasmu disini</p>
+                                        <p className='text-[19px] text-[#A7A7A7] font-Cairo'>atau</p>
+                                        <p className="text-[#5885E9] mt-[25px] font-bold border-2 border-[#5885E9] rounded-xl px-5 py-3">Pilih File</p>
                                     </div>
                                     <input id="dropzone-file" type="file" className="hidden" />
                                 </label>
@@ -146,7 +151,7 @@ export default function FourthStep() {
                         <div className='mt-[50px]'>
                             <label className='font-bold text-[16px]'>Catatan Untuk Tutor</label>
                             <div className='mb-6 mt-[30px] relative'>
-                                <div className='absolute inset-y-0 left-0 flex mt-[20px] pl-3 pointer-events-none'>
+                                <div className='absolute inset-y-0 left-0 flex mt-[10px] pl-3 pointer-events-none'>
                                     <FaPen className='text-[#B0B1B0] text-[18px]'/>
                                 </div>
                                 <textarea 
@@ -154,7 +159,7 @@ export default function FourthStep() {
                                     onChange={(e) => setUserData({...userData, "catatanTutor" : e.target.value})} 
                                     value= {userData['catatanTutor']}
                                     name='catatanTutor' 
-                                    className='border outline-none h-[200px] flex items-start focus:ring-2  focus:ring-[#3C64B1] w-[320px] pl-[40px] text-[13px] mt-[10px] border-[#B0B1B0] rounded-2xl p-2' 
+                                    className='border outline-none h-[200px] flex items-start focus:ring-2  focus:ring-[#3C64B1] w-full pl-[40px] text-[13px] mt-[10px] border-[#B0B1B0] rounded-2xl p-2' 
                                     required 
                                     placeholder='Tuliskan masukkanmu disini'/>
                             </div>
